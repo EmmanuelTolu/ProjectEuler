8ada3dc210c78dca23ca1bf907f0856dd5dab5b4
Emmanuel Adeyemo <91485892+Immaculate2099@users.noreply.github.com>
2021-10-18 08:14:44 +0100

    Add files via upload

--- largest_palindrome_3digit.py
+++ largest_palindrome_3digit.py
@@ -1,6 +1,5 @@
 #This program shows the largest palindrome of a 3 digit
-data1 = range(999, 100, -1)
-data2 = range(999, 100, -1)
+data1 = data2 = range(999, 100, -1)
 pal = []
 
 def largest_palindrome_3digit(data1, data2):
